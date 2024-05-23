ca6650376b5635cb45f2e6746b747735da6e640d
Fabio Fajardo <ffajardo64@github.com>
2024-05-23 08:49:39 -0300

    Atualizando layout do site visitas.html

--- js/main.js
+++ js/main.js
@@ -55,6 +55,39 @@
         navigationText: false
     });
 
+	$("#owl-slider1").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [700, 2],
+	        [960, 3]
+	     ],
+        navigationText: false
+    });
+
+	$("#owl-slider2").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [700, 2],
+	        [960, 3]
+	     ],
+        navigationText: false
+    });
+
+	$("#owl-slider3").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [700, 2],
+	        [960, 3]
+	     ],
+        navigationText: false
+    });
+	
 	/*---------------------------------------------------- */
 	/* Owl Carousel Portfolio
 	------------------------------------------------------ */ 
